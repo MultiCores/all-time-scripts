82ec9fdcc707dc0a6736c6409b1e1a8c6ff08c3e
MultiCores <49386650+MultiCores@users.noreply.github.com>
2021-05-10 16:25:00 +0300

    Updated MainMenu script.

--- WeDescend2021/MainMenu.cs
+++ WeDescend2021/MainMenu.cs
@@ -3,11 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // A function to simply load up the scene '1'.
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
-
+    
+    // A function to shut down the application.
     public void ExitGame()
     {
         Application.Quit();
