11c159f05363ab86029bcc2ccc2cb4b75e0efb96
MultiCores <49386650+MultiCores@users.noreply.github.com>
2021-05-10 16:27:16 +0300

    Updated SettingsMenu script.

--- WeDescend2021/SettingsMenu.cs
+++ WeDescend2021/SettingsMenu.cs
@@ -7,11 +7,15 @@ public class SettingsMenu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
+    
+    // Sets the volume to said float value in the element 'audioMixer'. 
+    // Simply made to transfer the audio volume between the mainmenu and ingame settings midst gameplay.
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
     }
-
+    
+    // Checks if fullscreen is on or off.
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
