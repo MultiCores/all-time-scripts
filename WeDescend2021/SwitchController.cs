6c92ce13b14118dbfd7269fcef5e50d57f6a510d
MultiCores <49386650+MultiCores@users.noreply.github.com>
2021-05-10 16:32:56 +0300

    Updated SwitchController script.

--- WeDescend2021/SwitchController.cs
+++ WeDescend2021/SwitchController.cs
@@ -8,6 +8,7 @@ public class SwitchController : MonoBehaviour
     public GameObject lever;
     public Animator anim;
 
+    // Sets the animation state names for the lever.
     private enum State {idle, turnLeft, turnRight}
     private State state = State.idle;
 
@@ -26,6 +27,8 @@ public class SwitchController : MonoBehaviour
 
     IEnumerator AnimationState()
     {
+        // Checks for user's location, input and the current animation state.
+        // Also starts the stageGenerator for the next level.
         if (Input.GetKeyDown(KeyCode.E) && state == State.idle && leverAccessible == true) {
             state = State.turnLeft;
             yield return new WaitForSeconds(0.6f);
@@ -35,7 +38,9 @@ public class SwitchController : MonoBehaviour
             }
             state = State.idle;
         }
-
+        
+        // Checks for user's location, input and the current animation state.
+        // Also refreshes the scene to load up the next playable level.
         else if (Input.GetKeyDown(KeyCode.Q) && state == State.idle) {
             state = State.turnRight;
             yield return new WaitForSeconds(0.6f);
@@ -49,6 +54,7 @@ public class SwitchController : MonoBehaviour
         yield break;
     }
 
+    // Checks if player is in range of a collider.
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.tag == "Player")
@@ -56,7 +62,8 @@ public class SwitchController : MonoBehaviour
             leverAccessible = true;
         }
     }
-
+    
+    // Checks if player isn't in range of a collider.
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Player")
